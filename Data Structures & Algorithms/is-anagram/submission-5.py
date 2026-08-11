c9e4639d16e6632a7c24eefed4e7f05e7733349e
Christian Paul <christianspaul72@gmail.com>
2026-08-11 14:45:44 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        smap = {}
+        for char in s:
+            if char in smap:
+                smap[char] += 1
+            else:
+                smap[char] = 1
+        tmap = {}
+        for char in t:
+            if char in tmap:
+                tmap[char] += 1
+            else:
+                tmap[char] = 1
+        return smap == tmap
